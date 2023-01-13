4682dff4156e90d6e314fa68262145ef9f316ad5
Zhibo-Li <zhibo.li.1994@gmail.com>
2023-01-13 14:59:07 +0100

    Change: interaction index definition.
    number of direct-contacts.
    number of perturbed cases.

--- Vic_RigidFiberPostprocessing_Simulation.m
+++ Vic_RigidFiberPostprocessing_Simulation.m
@@ -7,18 +7,24 @@ thedeg = cell2mat(xlsfile(:, 1));
 theL = round(cell2mat(xlsfile(:, 2)), 1); 
 they0 = round(cell2mat(xlsfile(:, 3)), 3); 
 
-obs = readVTK('D:\Dropbox\Collaboration - LadHyX\Give_to_Zhibo_nonShared\obstacle_beads.vtk');
-obs_beads_size = 1.5e-6; % notice here (radius)
+% obs = readVTK('D:\Dropbox\Collaboration - LadHyX\Give_to_Zhibo_nonShared\obstacle_beads.vtk');
+% obs_beads_size = 1.5e-6; % notice here (radius)
 fiber_beads_size = 0.8e-6 * 1.25; % notice here (radius)
-obs_2d = obs.points(1:267, 1:2); obs_2d_center = mean(obs_2d, 1);
-obs_2d = obs_2d * 8e-6;
-obs_2d_center = obs_2d_center * 8e-6;
-obs_2d(:, 1) = (obs_2d(:, 1) - obs_2d_center(1)) * ...
-    (2.5e-5 + obs_beads_size) / 2.5e-5 + obs_2d_center(1);
-obs_2d(:, 2) = (obs_2d(:, 2) - obs_2d_center(2)) * ...
-    (2.5e-5 + obs_beads_size) / 2.5e-5 + obs_2d_center(2); 
-%%% 2.5e-5 is the 1/3 the height of the equilateral triangle
-obs_2d = [obs_2d; obs_2d(1, :)];
+% obs_2d = obs.points(1:267, 1:2); obs_2d_center = mean(obs_2d, 1);
+% obs_2d = obs_2d * 8e-6;
+% obs_2d_center = obs_2d_center * 8e-6;
+% obs_2d(:, 1) = (obs_2d(:, 1) - obs_2d_center(1)) * ...
+%     (2.5e-5 + obs_beads_size) / 2.5e-5 + obs_2d_center(1);
+% obs_2d(:, 2) = (obs_2d(:, 2) - obs_2d_center(2)) * ...
+%     (2.5e-5 + obs_beads_size) / 2.5e-5 + obs_2d_center(2); 
+% %%% 2.5e-5 is the 1/3 the height of the equilateral triangle
+% obs_2d = [obs_2d; obs_2d(1, :)];
+load(['F:\Processing & Results\FSI - Rigid Fiber &  Individual Obstacle\' ...
+    'Figures\about interaction index\obs_2d.mat']);
+load(['F:\Processing & Results\FSI - Rigid Fiber &  Individual Obstacle\' ...
+    'Figures\about interaction index\pert_cont (50%).mat'])
+l_obstacle = 86.6; % um
+time_step = 0.01; % s
 
 parent_path = 'D:\Dropbox\Collaboration - LadHyX\Give_to_Zhibo_nonShared\data';
 sub1_path = dir(parent_path);
@@ -38,52 +44,47 @@ for sub1Path_i = 3:length(sub1_path)
             y0_num = str2double(newStr(4:end));
             fileinfo = dir(fullfile(parent_path, current_deg, current_L, current_y0, 'output_data\*.vtk'));
 
-            circle_intersec = 0; line_intersec = 0;
+            perturb_contact = 0; direct_contact = 0;
             for ii = 1:length(fileinfo)
                 snapshot = readVTK(fullfile(fileinfo(ii).folder, fileinfo(ii).name));
-                XY = snapshot.points(:, 1:2); 
-                CoM(:, ii) = mean(XY, 1)'; 
-%                 Half_L = sqrt((XY(1,1)-XY(end,1))^2 + (XY(1,2)-XY(end,2))^2) / 2 + fiber_beads_size;
-% 
-%                 theta = linspace(0,2*pi,300);
-%                 circle_x = Half_L*cos(theta); circle_x = circle_x + CoM(1, ii);
-%                 circle_y = Half_L*sin(theta); circle_y = circle_y + CoM(2, ii);
-% 
-%                 [intersec_cir_x, intersec_cir_y] = polyxpoly(obs_2d(:,1), obs_2d(:,2), circle_x, circle_y);
-%                 if ~isempty(intersec_cir_y)
-                if min(pdist2(CoM(:, ii)',obs_2d,'euclidean','Smallest',1)) * 1e6 < 40
-                    circle_intersec = circle_intersec + 1; 
-                    if min(pdist2(XY,obs_2d,'euclidean','Smallest',1)) < fiber_beads_size
-                        line_intersec = line_intersec + 1;
-                    end
+                XY = snapshot.points(:, 1:2);
 
-%                     [intersec_line_x, intersec_line_y] = polyxpoly(obs_2d(:,1), obs_2d(:,2), XY(:,1), XY(:,2));
-%                     if ~isempty(intersec_line_y)
-%                         line_intersec = line_intersec + 1;
-%                     end
-                end 
-            end
-            if circle_intersec == 0
-                interaction3 = 0;
-            else
-                interaction3 = line_intersec / circle_intersec;
+                if_direct_contect = min(pdist2(XY,obs_2d,'euclidean','Smallest',1)) < fiber_beads_size; % if it's direct contact.
+                if if_direct_contect
+                    direct_contact = direct_contact + 1;
+                end
+
+%                 in_triangle = inpolygon(XY(:,1),XY(:,2),obs_2d(:,1),obs_2d(:,2)); % the points on the fiber which are inside the triangular obstacle
+%                 in_perturb_C1 = inpolygon(XY(:,1),XY(:,2),pert_C1(:,1),pert_C1(:,2)); % the points on the fiber which are inside the perturbed line 1
+%                 if_inbetween_twolines = and(~logical(in_triangle), logical(in_perturb_C1)); % the fiber is in between the two contours
+%                 if_inbetween_twolines = logical(sum(if_inbetween_twolines));
+% 
+%                 if min(pdist2(XY,pert_C1,'euclidean','Smallest',1)) < fiber_beads_size || ...
+%                     if_direct_contect || if_inbetween_twolines
+%                     perturb_contact = perturb_contact + 1;
+%                 end
+
+                in_perturb_C2 = inpolygon(XY(:,1),XY(:,2),pert_C2(:,1),pert_C2(:,2)); % the points on the fiber which are inside the perturbed line 2
+                if_in_C2 = logical(sum(logical(in_perturb_C2)));
+
+                if (min(pdist2(XY,pert_C2,'euclidean','Smallest',1)) < fiber_beads_size || if_in_C2) && ~if_direct_contect % counting number of perturbed contact
+                    perturb_contact = perturb_contact + 1;
+                end
+
             end
 
-%             if mean(CoM(2, and(obs_2d_center(1)-3e-5 < CoM(1, :), CoM(1, :) < obs_2d_center(1)+3e-5))) > obs_2d_center(2) % if it goes below (in image system).
-%                 bypass_tip = true;
-%             else
-%                 bypass_tip = false;
-%             end
-
-%             dx = diff(CoM(1, :))'; % [x(i+1) - x(i)]
-%             U0 = 0.5 * (CoM(1, 2)-CoM(1, 1) + CoM(1, end)-CoM(1, end-1)); % U0: average of speed_upstream and speed_downstream
-%             interaction1 = 1-mean(dx)/U0;
-%             interaction2 = max(abs(dx-U0))/U0; % interaction2 defines as max(abs(U0-U(t)))/U0;
+            interaction1 = perturb_contact * time_step / (l_obstacle / U_max_phy);
+            interaction2 = direct_contact * time_step / (l_obstacle / U_max_phy);
+%             interaction3 = interaction1 + interaction2;
 
             tmp_index =  thedeg==deg_num & theL==L_num & they0==y0_num;
             Ind = find(tmp_index==1);
-            Loc = ['Q', num2str(Ind+1)];  % The locations in the excel should be written into. (+1 because there is headerline in the excel.)
-            writematrix(interaction3,[excelpathname, excelname],'Sheet','Sheet1','Range', Loc);  % Write the value inti the excel.
+            Loc = ['AD', num2str(Ind+1)];  % The locations in the excel should be written into. (+1 because there is headerline in the excel.)
+            writematrix(interaction1,[excelpathname, excelname],'Sheet','Sheet1','Range', Loc);  % Write the value inti the excel.
+            Loc = ['AE', num2str(Ind+1)];  % The locations in the excel should be written into. (+1 because there is headerline in the excel.)
+            writematrix(interaction2,[excelpathname, excelname],'Sheet','Sheet1','Range', Loc);  % Write the value inti the excel.
+%             Loc = ['X', num2str(Ind+1)];  % The locations in the excel should be written into. (+1 because there is headerline in the excel.)
+%             writematrix(interaction3,[excelpathname, excelname],'Sheet','Sheet1','Range', Loc);  % Write the value inti the excel.
             
         end
     end
@@ -94,9 +95,10 @@ end
 %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% plot %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+clear; close all; clc;
 xlsfile = readcell(['D:\Dropbox\Collaboration - LadHyX\Give_to_Zhibo_nonShared\' ...
     'results_more-data.xlsx'],'Sheet','Sheet1','NumHeaderLines',1); 
-together_plot = [cell2mat(xlsfile(:, 1:4)), cell2mat(xlsfile(:, 13)), cell2mat(xlsfile(:, 17))]; 
+together_plot = [cell2mat(xlsfile(:, 1:4)), cell2mat(xlsfile(:, 13)), cell2mat(xlsfile(:, 30))]; 
 together_plot = together_plot';
 % thedeg = cell2mat(xlsfile(:, 1)); 
 % theL = round(cell2mat(xlsfile(:, 2)), 1); 
@@ -150,11 +152,14 @@ scatter(pole_vaulting_together(6, :), pole_vaulting_together(4, :), 220, pole_va
 hcb=colorbar; caxis([0 1])
 title(hcb,'$Initial\ position\ (y_0/h_{obs})$','FontSize', 16,'Interpreter', 'latex'); grid on
 set(gca,'FontSize',16);
-xlabel('$max\left|U_0-U(t)\right|/U_0$','FontSize', 22,'Interpreter', 'latex');
-xlabel('$Contact\ probability\ (disturbed\ layer = 40\mu{m}) $','FontSize', 22,'Interpreter', 'latex');  % for interaction3
-% ylabel('$Deviation\ (\delta/h_{obs})$','FontSize', 22,'Interpreter', 'latex');
-legend({'Trapping','Below','Above','Pole-vaulting'}, 'Location', 'southwest','FontSize', 14,'Interpreter', 'latex')
+% xlabel('$max\left|U_0-U(t)\right|/U_0$','FontSize', 22,'Interpreter', 'latex');
+% xlabel('$Contact\ probability\ (disturbed\ layer = 40\mu{m}) $','FontSize', 22,'Interpreter', 'latex');  % for interaction3
+xlabel('$Normalized\ direct\ contact\ duration$','FontSize', 22,'Interpreter', 'latex');  
+% xlabel('$Normalized\ perturbed\ duration$','FontSize', 22,'Interpreter', 'latex'); 
+% xlabel('$Interaction\ index$','FontSize', 22,'Interpreter', 'latex'); 
+ylabel('$Deviation\ (\delta/h_{obs})$','FontSize', 22,'Interpreter', 'latex');
+legend({'Trapping','Below','Above','Pole-vaulting'}, 'Location', 'northeast','FontSize', 14,'Interpreter', 'latex')
 % title('$0.5<L<1$','FontSize', 22,'Interpreter', 'latex')
-% xlim([0 1]); ylim([-0.4 0.8])
-f=gcf;
-exportgraphics(f,'y0_vs_contactprobability(layer50um)-delta_simulation_chi0-m10to10.png','Resolution',100)
+% xlim([0 12]); %ylim([-0.4 0.8])
+% f=gcf;
+% exportgraphics(f,'F:\Processing & Results\FSI - Rigid Fiber &  Individual Obstacle\Figures\about interaction index\direct contact duration.png','Resolution',100)
