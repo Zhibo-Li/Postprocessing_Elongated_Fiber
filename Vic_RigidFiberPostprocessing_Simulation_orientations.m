97e7f84df403b0f2b71311617ef01e498cd3d54e
Zhibo-Li <zhibo.li.1994@gmail.com>
2023-04-17 17:18:38 +0200

    Add: section of plotting orientations vs x-positions for the paper (with an inset).

--- Vic_RigidFiberPostprocessing_Simulation_orientations.m
+++ Vic_RigidFiberPostprocessing_Simulation_orientations.m
@@ -632,3 +632,100 @@ for choose_y0 = [0.1:0.1:0.9, 0.15:0.1:0.55, 0.325:0.05:0.525]
 end
 
 
+
+%% orientations vs x: given y_0 (0.425) and theta_0 (-7.5) to plot different L (until contact)
+clear; close all; clc;
+
+Files = dir(['D:\Dropbox\Collaboration - LadHyX\Give_to_Zhibo_nonShared' ...
+    '\Data_Give_to_Zhibo_20230223_videos\Orientations\until contact\*.mat']);
+
+for choose_y0 = 0.425
+    for choose_theta0 = -7.5
+
+        n = 1;
+        for ii = 1: length(Files)
+
+            if ~exist(fullfile(Files(ii).folder, Files(ii).name), 'file')
+                continue
+            else
+                load(fullfile(Files(ii).folder, Files(ii).name));
+                current_deg =  extractBetween(Files(ii).name,'theta0_', '_L');
+                current_deg = strrep(current_deg,'o','.'); current_deg = strrep(current_deg,'m','-');
+                current_L =  extractBetween(Files(ii).name,'L_', '_y0');
+                current_L = strrep(current_L,'o','.');
+                current_y0 =  extractBetween(Files(ii).name,'y0_', '_ori');
+                current_y0 = strrep(current_y0,'o','.');
+                if isnan(str2double(current_y0{1}))
+                    current_y0_tmp = current_y0{1};
+                    current_y0{1} = current_y0_tmp(1:end-10);
+                end
+
+                if str2double(current_y0) ~= choose_y0 || str2double(current_deg) ~= choose_theta0
+                    % choose the y_0 and L to be plotted
+                    continue
+                else
+                    title_y0 = current_y0;
+                    title_deg = current_deg;
+                    To_Plot{n, 1} = ori_ee_untilContact;
+                    To_Plot{n, 2} = x_untilContact;
+                    To_Plot{n, 3} = str2double(current_L); % to be sorted and plotted
+                    n = n + 1;
+                end
+
+            end
+        end
+
+        fig1 = figure('color', 'w'); set(gcf, 'Position', [100 100 800 600]);
+        cmap = cmocean('matter');  legend_txt = {};
+        To_Plot([1,3,5,7], :) = []; % only plot L=0.6, 0.8, 1 and 1.4.
+        [~, sortID] = sort(cell2mat(To_Plot(:,3)));  % sort the plotting order
+
+        color_ind = size(To_Plot, 1);
+        for jj = 1:size(To_Plot, 1)
+
+            plot(To_Plot{sortID(jj), 2}, To_Plot{sortID(jj), 1}, 'o','MarkerSize', 8,'MarkerEdgeColor','k', ...
+                'MarkerFaceColor', cmap(color_ind*60,:)); hold on
+
+            xlabel('$x/h_{obs}$','FontSize', 24,'Interpreter', 'latex');
+            ylabel('$\Delta\theta$','FontSize', 24,'Interpreter', 'latex');
+
+            color_ind = color_ind - 1; % coincidence with the vector plot
+
+        end
+        xlim([-5 0])
+        set(gca, ...
+            'Box', 'On', ...
+            'XGrid', 'On', ...
+            'YGrid', 'On', ...
+            'GridAlpha', 0.5, ...
+            'FontSize', 24, ...
+            'NextPlot','replacechildren', ...
+            'TickLabelInterpreter','latex')
+
+        axes('Position',[.25 .3 .4 .3])
+
+        color_ind = size(To_Plot, 1);
+        for jj = 1:size(To_Plot, 1)
+
+            select_To_Plot = To_Plot{sortID(jj), 2};
+            select_ind = select_To_Plot < -4;
+            plot(To_Plot{sortID(jj), 2}(select_ind), To_Plot{sortID(jj), 1}((select_ind)), ...
+                'o','MarkerSize', 8,'MarkerEdgeColor','k', 'MarkerFaceColor', cmap(color_ind*60,:)); hold on
+
+            color_ind = color_ind - 1; % coincidence with the vector plot
+
+        end
+        xlim([-5 -4]); ylim([-0.4, 0])
+
+        savename = strcat('y_0=', title_y0, '_theta_0=', title_deg, '_orientation_vs_x.eps');
+
+        set_plot(gcf, gca) 
+%         f=gcf;
+%         exportgraphics(f,['F:\Processing & Results\FSI - Rigid Fiber &  Individual Obstacle\' ...
+%             'Figures\about orientations vs x\until contact\Given y0 and theta0\', savename{1}]
+
+%         close
+    end
+end
+
+
