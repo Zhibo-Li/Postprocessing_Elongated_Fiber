6175bb9d465fd8b9a7cbf2f4d0f5e7b57de661a3
Zhibo-Li <zhibo.li.1994@gmail.com>
2023-05-30 14:39:23 +0200

    Delete: useless part.

--- Vic_ActinRecurrencePlot.m
+++ Vic_ActinRecurrencePlot.m
@@ -263,30 +263,3 @@ f=gcf;
 exportgraphics(f,['F:\Processing & Results\Actin Filaments in Porous Media\Figures\Poincare plots\Simu_theta=',num2str(Simu_deg),'_tracer.png'],'Resolution',100)
 
 
-%% functions
-function L_0 = Vic_Get_ave_cutExtreme(spl_Ls, threshold)
-% the function is used to calculte the contour lenght of the filament based
-% on the average without the extrame values.
-%
-% threshold -- threshold to truncate the extreme values
-
-L_0_coarse = mean(spl_Ls, 'omitnan'); % roughly estimated filament length
-spl_Ls(spl_Ls < L_0_coarse*(1-threshold)) = [];
-spl_Ls(spl_Ls > L_0_coarse*(1+threshold)) = []; % remove the extreme value
-if isempty(spl_Ls)
-    L_0 = L_0_coarse;
-    return
-end
-L_0 = mean(spl_Ls, 'omitnan');
-
-while L_0_coarse ~= L_0 && ~isnan(L_0)
-    L_0_coarse = L_0;
-    spl_Ls(spl_Ls < L_0_coarse*(1-threshold)) = [];
-    spl_Ls(spl_Ls > L_0_coarse*(1+threshold)) = []; % remove the extreme value
-    if isempty(spl_Ls)
-        L_0 = L_0_coarse;
-        return
-    end
-    L_0 = mean(spl_Ls, 'omitnan');
-end
-end
