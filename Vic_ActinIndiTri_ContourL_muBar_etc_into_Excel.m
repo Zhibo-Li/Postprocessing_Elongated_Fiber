a0364f22ec0104e3ca9a4dcfc212a001bfe7c8c2
Zhibo-Li <zhibo.li.1994@gmail.com>
2023-06-30 15:28:12 +0200

    Change: correct the calculation (it was wrong due to the difference betweeen image and matrix coordinates).

--- Vic_ActinIndiTri_ContourL_muBar_etc_into_Excel.m
+++ Vic_ActinIndiTri_ContourL_muBar_etc_into_Excel.m
@@ -25,12 +25,14 @@ xlsfile_to_write = readcell([excelpathname, excelname],'Sheet','Sheet1','NumHead
 correct_ind = find(cellfun(@(x) contains(x, 'PlusInfo_Slope_correction'), {Files.name}));
 load(fullfile(Files(1).folder, Files(correct_ind).name));
 % calculate the information of the obstacle
+obs_2d(:, 2) = 2048 - obs_2d(:, 2); % !!!!!! Now, it's in matrix coordinates and the triangle pointing down !!!.
+% !!!!!! and the filament data is also in matrix coordinates !
 obs_center_xy = mean(obs_2d);
 D = sort(pdist2(obs_center_xy, obs_2d));  % distances from center to edge (sorted);
 obs_h = mean(D(1:round(0.01*length(D)))) + mean(D(end-round(0.01*length(D)):end));
 % obstacle altitude is sum of 1% shortest average and 1% longest average values.
-obs_base_y = obs_center_xy(2) - mean(D(1:round(0.01*length(D))));
-% obstacle base_y is center_y minus 1% shortest average.
+obs_base_y = obs_center_xy(2) + mean(D(1:round(0.01*length(D))));
+% obstacle base_y is center_y plus 1% shortest average.
 
 left_border = obs_center_xy(1) - 3.5*obs_h;
 right_border = obs_center_xy(1) + 3.5*obs_h; % give range for y_0 and delta calculation
@@ -62,16 +64,16 @@ for file_ind = 1:length(Files)
             y_0 = nan; y_f = nan; theta_0 = nan;
         end
 
-        delta = (y_f - y_0) - delta_correct;
+        delta = -((y_f - y_0) - delta_correct); % delta is respect to the triangle pointing up.
         
         Loc = ['B', num2str(excel_pos+1)];  % The locations in the excel should be written into. (+1 because there is headerline in the excel.)
         writematrix(ContourL,[excelpathname, excelname],'Sheet','Sheet1','Range', Loc);  % Write the value inti the excel.
         Loc = ['C', num2str(excel_pos+1)];  % The locations in the excel should be written into. (+1 because there is headerline in the excel.)
-        writematrix((y_0 - obs_base_y)/obs_h,[excelpathname, excelname],'Sheet','Sheet1','Range', Loc);  % Write the value inti the excel.
+        writematrix((obs_base_y - y_0)/obs_h,[excelpathname, excelname],'Sheet','Sheet1','Range', Loc);  % Write the value inti the excel.
         Loc = ['D', num2str(excel_pos+1)];  % The locations in the excel should be written into. (+1 because there is headerline in the excel.)
         writematrix(delta/obs_h,[excelpathname, excelname],'Sheet','Sheet1','Range', Loc);  % Write the value inti the excel.
         Loc = ['E', num2str(excel_pos+1)];  % The locations in the excel should be written into. (+1 because there is headerline in the excel.)
-        writematrix(atand(theta_0),[excelpathname, excelname],'Sheet','Sheet1','Range', Loc);  % Write the value inti the excel.
+        writematrix(-atand(theta_0),[excelpathname, excelname],'Sheet','Sheet1','Range', Loc);  % Write the value inti the excel.
         
     end
 end
