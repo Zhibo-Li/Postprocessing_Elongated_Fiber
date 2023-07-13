452b07eecd2a4ddca431e70ffdb9817127637ae5
Zhibo-Li <zhibo.li.1994@gmail.com>
2023-07-13 13:59:15 +0200

    Major changes:
    1. calculate 'contact probability';
    2. plot 'f_y' and 'contact probability' vs. delta.

--- Vic_RigidFiberPostprocessing_Simulation_contactforce_test.m
+++ Vic_RigidFiberPostprocessing_Simulation_contactforce_test.m
@@ -31,9 +31,9 @@ obs_bottom = min(obs_2d(:,2));
 
 parent_path = 'D:\Dropbox\Collaboration - LadHyX\Give_to_Zhibo_nonShared\Data_Give_to_Zhibo_20230223\simulations';
 sub1_path = dir(parent_path);
-for sub1Path_i = 7
-    current_deg = sub1_path(sub1Path_i).name; 
-    newStr = strrep(current_deg,'o','.'); newStr = strrep(newStr,'m','-'); 
+for sub1Path_i = 3:length(sub1_path)
+    current_deg = sub1_path(sub1Path_i).name;
+    newStr = strrep(current_deg,'o','.'); newStr = strrep(newStr,'m','-');
     deg_num = str2double(newStr(8:end));
     sub2_path = dir(fullfile(parent_path, current_deg));
     for sub2Path_i = 3:length(sub2_path)
@@ -79,39 +79,98 @@ for sub1Path_i = 7
             Loc = ['L', num2str(Ind+1)];  % The locations in the excel should be written into. (+1 because there is headerline in the excel.)
             writematrix(impulse_time_integral,[excelpathname, excelname],'Sheet','Sheet1','Range', Loc);  % Write the value into the excel.
 
-%             Loc = ['K', num2str(Ind+1)];  % The locations in the excel should be written into. (+1 because there is headerline in the excel.)
-%             writematrix(mean_force,[excelpathname, excelname],'Sheet','Sheet1','Range', Loc);  % Write the value into the excel.
+            Loc = ['K', num2str(Ind+1)];  % The locations in the excel should be written into. (+1 because there is headerline in the excel.)
+            writematrix(mean_force,[excelpathname, excelname],'Sheet','Sheet1','Range', Loc);  % Write the value into the excel.
 
-            clearvars impulse_time_integral  
+            % interaction index 3 (defines as contact probability -- varying layer thickness)
+            circle_intersec = 0; line_intersec = 0;
+            for jj = 1:length(fileinfo)
+
+                snapshot = readVTK(fullfile(fileinfo(jj).folder, fileinfo(jj).name));
+                XY = snapshot.points(:, 1:2);
+                Half_L = sqrt((XY(1,1)-XY(end,1))^2 + (XY(1,2)-XY(end,2))^2) / 2 + 0.593 * (obs_beads_size+fiber_beads_size);
+
+                theta = linspace(0,2*pi,300);
+                circle_x = Half_L*cos(theta); circle_x = circle_x + mean(XY(:, 1));
+                circle_y = Half_L*sin(theta); circle_y = circle_y + mean(XY(:, 2));
+                [intersec_cir_x, intersec_cir_y] = polyxpoly(obs_2d(:,1), obs_2d(:,2), circle_x, circle_y);
+                if ~isempty(intersec_cir_y)
+                    circle_intersec = circle_intersec + 1;
+                    if min(pdist2(XY,obs_2d,'euclidean','Smallest',1)) < 0.593 * (obs_beads_size+fiber_beads_size)
+                        line_intersec = line_intersec + 1;
+                    end
+                end
+            end
+            if circle_intersec == 0
+                interaction3 = 0;
+            else
+                interaction3 = line_intersec / circle_intersec;
+            end
+
+            tmp_index =  thetheta0==deg_num & theL==L_num & ismembertol(they0, y0_num,0.0125);
+            Ind = find(tmp_index==1);
+
+            Loc = ['M', num2str(Ind+1)];  % The locations in the excel should be written into. (+1 because there is headerline in the excel.)
+            writematrix(interaction3 ,[excelpathname, excelname],'Sheet','Sheet1','Range', Loc);  % Write the value into the excel
+
+            clearvars interaction3
 
         end
     end
 end
 
 
-%% plot
+%% plot steric force vs delta
 clear; close all; clc;
 
 xlsfile = readcell(['D:\Dropbox\Collaboration - LadHyX\' ...
     'Give_to_Zhibo_nonShared\Data_Give_to_Zhibo_20230223\' ...
     'results_2023_02_23_test.xlsx'],'Sheet','Sheet1','NumHeaderLines',1); 
 
-thedyn = cell2mat(xlsfile(:, 5));
+thedyn = cell2mat(xlsfile(:, 5)); % the dynamics: 3 is trapping
 
-theL = cell2mat(xlsfile(:, 2)); %theL(thedyn==3) = [];
-thedelta = cell2mat(xlsfile(:, 4)); thedelta(thedyn==3) = [];
-theimpulse = cell2mat(xlsfile(:, 9)); %theimpulse(thedyn==3) = [];
-theforce = cell2mat(xlsfile(:, 11)); %theforce(thedyn==3) = [];
-theforce_times_t = cell2mat(xlsfile(:, 12)); theforce_times_t(thedyn==3) = [];
+thedelta = cell2mat(xlsfile(:, 4)); % thedelta(thedyn==3) = [];
+theforce = cell2mat(xlsfile(:, 11)); % theforce(thedyn==3) = [];
 
+figure('color', 'w'); set(gcf, 'Position', [100 100 800 600]);
+plot(theforce / max(abs(theforce)), thedelta, '.k', 'MarkerSize', 15)
 
-% figure; plot(theforce, 'ro')
-% figure; plot(thedelta, 'b*')
-figure; plot(theforce_times_t, thedelta, 'm^')
-% xlim([-0.5e-4 0.5e-4])
+xlim([-0.1 1.1]); ylim([-0.3 0.7]);
+xlabel('$f_y/|f_y|_{\rm max}$','FontSize', 24,'Interpreter', 'latex'); 
+ylabel('$\delta/h_{\rm obs}$','FontSize', 24,'Interpreter', 'latex');
+set(gca,'Box', 'On','XGrid', 'On','YGrid', 'On','FontSize', 24)
 
-% xlabel('$Steric\ force\ (f_y)$','FontSize', 22,'Interpreter', 'latex'); 
-xlabel('$\int f_y t$','FontSize', 22,'Interpreter', 'latex'); 
-% xlabel('$Interaction\ index$','FontSize', 22,'Interpreter', 'latex'); 
-ylabel('$Deviation\ (\delta/h_{\rm obs})$','FontSize', 22,'Interpreter', 'latex');
-set_plot(gcf, gca)
+hhh = gcf;
+set(hhh,'Units','Inches');
+pos = get(hhh,'Position');
+set(hhh,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+print(hhh, '-dpdf',['F:\Processing & Results\FSI - Rigid Fiber &  Individual ' ...
+    'Obstacle\Figures\about interaction index\F_y_delta_simu.pdf']);
+
+%% plot interaction index vs delta
+clear; close all; clc;
+
+xlsfile = readcell(['D:\Dropbox\Collaboration - LadHyX\' ...
+    'Give_to_Zhibo_nonShared\Data_Give_to_Zhibo_20230223\' ...
+    'results_2023_02_23_test.xlsx'],'Sheet','Sheet1','NumHeaderLines',1); 
+
+thedyn = cell2mat(xlsfile(:, 5)); % the dynamics: 3 is trapping
+
+thedelta = cell2mat(xlsfile(:, 4)); % thedelta(thedyn==3) = [];
+contact_prob = cell2mat(xlsfile(:, 13)); % theforce(thedyn==3) = [];
+
+figure('color', 'w'); set(gcf, 'Position', [100 100 800 600]);
+plot(contact_prob, thedelta, '.k', 'MarkerSize', 15)
+
+xlim([-0.1 1.1]); ylim([-0.3 0.7]);
+xlabel('Contact probability','FontSize', 24,'FontName', 'Times New Roman'); 
+ylabel('$\delta/h_{\rm obs}$','FontSize', 24,'Interpreter', 'latex');
+set(gca,'Box', 'On','XGrid', 'On','YGrid', 'On','FontSize', 24)
+
+hhh = gcf;
+set(hhh,'Units','Inches');
+pos = get(hhh,'Position');
+set(hhh,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+print(hhh, '-dpdf',['F:\Processing & Results\FSI - Rigid Fiber &  Individual ' ...
+    'Obstacle\Figures\about interaction index\Contact_probability_delta_simu.pdf']);
+
