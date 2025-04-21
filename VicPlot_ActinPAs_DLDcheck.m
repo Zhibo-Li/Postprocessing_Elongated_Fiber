bd38bb288bfc6e1963b2bba8e379f57c23203a17
Zhibo LI <zhibo.li.1994@gmail.com>
2025-04-21 15:30:39 +0200

    Change: for new experiments.
    Some old data lost due to the damage of the hard disk.

--- VicPlot_ActinPAs_DLDcheck.m
+++ VicPlot_ActinPAs_DLDcheck.m
@@ -4,24 +4,44 @@ clear; close all; clc;
 
 image_height = 204.8; % the height of the image, unit: um 
 magni = 0.1; % the magnification of the objective (um/pixel)
+flowfocusing_bandwidth = 100; % the width of suspension band after flow-focusing, unit: um
 
 set(0, 'defaulttextinterpreter','latex');  
 set(groot, 'defaultAxesTickLabelInterpreter','latex');  
 set(groot, 'defaultLegendInterpreter','latex');
 
-fiber_lengths = []; fiber_Ys = [];
-for exp_date = 1:2 % 3
+fiber_lengths = []; fiber_Ys = []; fiber_Xs = [];
+for exp_date =  5 % 1:2 % 3
+
+    if exp_date == 1 || exp_date == 2 || exp_date == 4
+        lambda = 30; % center-to-center distance of pillars in pillar array
+    else 
+        lambda = 45; % center-to-center distance of pillars in pillar array
+    end
 
     switch exp_date
-        case 1
+        case 1 % data lost
             exp2proc = dir(['F:\Processing & Results\Actin Filaments in Porous Media\' ...
                 '20231030-Actin-DLD\Y*']);
-        case 2
+        case 2 % data lost
             exp2proc = dir(['F:\Processing & Results\Actin Filaments in Porous Media\' ...
                 '20240415-Actin-DLD\Y*']);
-        case 3
+        case 3 % data lost
             exp2proc = dir(['F:\Processing & Results\Actin Filaments in Porous Media\' ...
                 '20240522-Actin-DLD\Y*']);
+        case 4
+            exp2proc = dir(['E:\Processing & Results\Actin Filaments in Porous Media\' ...
+                '20241031-Actin-DLD\Y*']);
+            X_base = 6000; % unit: um
+            % load extracted data from *.csv files
+            longfibers = readmatrix(['E:\Processing & Results\' ...
+                'Actin Filaments in Porous Media\20241031-Actin-DLD\' ...
+                'Extracted_data_Y-positon_Length_exp20231030_exp20240415.csv']);
+        case 5
+            exp2proc = dir(['E:\Processing & Results\Actin Filaments in Porous Media\' ...
+                '20241111-Actin-DLD\Y*']);
+            X_base = 6000; % unit: um
+            
     end
 
     for ii = 1:length(exp2proc)
@@ -47,15 +67,25 @@ for exp_date = 1:2 % 3
                 fiber_CoM_xy = mean(fiber_xy,1)*magni;
 
                 fiber_Ys = [fiber_Ys; image_height-fiber_CoM_xy(2)+Y_base];
+                fiber_Xs = [fiber_Xs; fiber_CoM_xy(1)+X_base];
 
             end
         end
     end
+
+    if exp_date == 4
+        fiber_Xs = [fiber_Xs; ones(size(longfibers(:,2)))*9000];
+        fiber_Ys = [fiber_Ys; longfibers(:,2)];
+        fiber_lengths = [fiber_lengths; longfibers(:,1)];
+
+        data_num = size(longfibers(:,1), 1);
+    end
+
 end
 
 %% plot 1
 figure('Color','w','Position',[100 100 800 600]); 
-plot(fiber_lengths,fiber_Ys, 'r.', 'MarkerSize', 28);
+plot(fiber_lengths/45,fiber_Ys, 'r.', 'MarkerSize', 28);
 ylabel('$y\ (\mathrm{\mu m})$','FontSize', 24,'Interpreter', 'latex');
 xlabel('$L\ (\mathrm{\mu m})$','FontSize', 24,'Interpreter', 'latex');
 set(gca,'Box', 'On','XGrid', 'On', 'YGrid', 'On', 'GridAlpha', 0.5, 'FontSize', 24)
@@ -102,67 +132,168 @@ print('-depsc2','-tiff','-r100','-vector',['F:\Processing & Results\' ...
     'Actin Filaments in Porous Media\Figures\Actin-DLD\Y-positon_Length_violinPlot.eps']);
 
 
-%% errorbar plot
+%% errorbar plot (bin average)
+figure('Color','w','Position',[100 100 800 600]);
+fiber_Ys_max = fiber_Ys;
+fiber_Ys_min = fiber_Ys - flowfocusing_bandwidth;
+fiber_Ys_min(end-data_num+1:end) = fiber_Ys(end-data_num+1:end) - 200; % different band width: 200 um
+fiber_Ys_min(fiber_Ys_min < 0) = 0; % set value to zeros if it's negative
+beta_max = atand(fiber_Ys_max./fiber_Xs);
+beta_min = atand(fiber_Ys_min./fiber_Xs);
+
+plot(fiber_lengths/lambda, beta_max, 'color', [179, 226, 205]/255, ...
+    'LineStyle', 'none', 'Marker', 'o', 'MarkerSize', 6, 'LineWidth', 1.5);
+hold on;
+
 n = 1;
-for ii = 5:10:100
+for ii = 5:5:100
 
     if ii < 80
-        YY = fiber_Ys(fiber_lengths > ii & fiber_lengths < ii+10);
+        YY_max = fiber_Ys_max(fiber_lengths > ii & fiber_lengths < ii+10);
+        YY_min = fiber_Ys_min(fiber_lengths > ii & fiber_lengths < ii+10);
+        XX = fiber_Xs(fiber_lengths > ii & fiber_lengths < ii+10);
         LL = fiber_lengths(fiber_lengths > ii & fiber_lengths < ii+10);
     else
-        YY = fiber_Ys(fiber_lengths > ii);
+        YY_max = fiber_Ys_max(fiber_lengths > ii);
+        YY_min = fiber_Ys_min(fiber_lengths > ii);
+        XX = fiber_Xs(fiber_lengths > ii);
         LL = fiber_lengths(fiber_lengths > ii);
     end
+    Y_max_mean(n) = mean(YY_max); Y_max_std(n) = std(YY_max);
+    Y_min_mean(n) = mean(YY_min); Y_min_std(n) = std(YY_min);
+    X_mean(n) = mean(XX); X_std(n) = std(XX);
     L_mean(n) = mean(LL); L_std(n) = std(LL);
-    Y_mean(n) = mean(YY); Y_std(n) = std(YY);
     n=n+1;
 
 end
 
-figure('Color','w','Position',[100 100 800 600]); 
-plot(fiber_lengths/20, atand(fiber_Ys/9000), 'm.', 'MarkerSize', 12);
+errorbar(L_mean/lambda, atand(Y_max_mean./X_mean), atand(Y_max_std./X_mean), ...
+    atand(Y_max_std./X_mean), L_std/lambda, L_std/lambda, 'color', [27, 158, 119]/255, ...
+        'LineStyle', 'none', 'Marker', 'o', 'MarkerSize', 10, 'LineWidth', 2);
 hold on;
-errorbar(L_mean/20, atand(Y_mean/9000), atand(Y_std/9000), atand(Y_std/9000), L_std/20, L_std/20, 'ok', 'LineStyle', ...
+% % % errorbar(L_mean/lambda, atand(Y_min_mean./X_mean), atand(Y_min_std./X_mean), ...
+% % %     atand(Y_min_std./X_mean), L_std/lambda, L_std/lambda, 'ok', 'LineStyle', ...
+% % %     'none', 'LineWidth',2);
+% % % hold on;
+
+
+%%% errorbar plot: overlap on the previous plot 
+fiber_Ys_max = fiber_Ys;
+fiber_Ys_min = fiber_Ys - flowfocusing_bandwidth;
+fiber_Ys_min(fiber_Ys_min < 0) = 0; % set value to zeros if it's negative
+beta_max = atand(fiber_Ys_max./fiber_Xs);
+beta_min = atand(fiber_Ys_min./fiber_Xs);
+
+plot(fiber_lengths/lambda, beta_max, 'color', [253, 205, 172]/255, ...
+    'LineStyle', 'none', 'Marker', '^', 'MarkerSize', 6, 'LineWidth', 1.5);
+hold on;
+
+n = 1;
+for ii = 5:5:100
+
+    if ii < 80
+        YY_max = fiber_Ys_max(fiber_lengths > ii & fiber_lengths < ii+10);
+        YY_min = fiber_Ys_min(fiber_lengths > ii & fiber_lengths < ii+10);
+        XX = fiber_Xs(fiber_lengths > ii & fiber_lengths < ii+10);
+        LL = fiber_lengths(fiber_lengths > ii & fiber_lengths < ii+10);
+    else
+        YY_max = fiber_Ys_max(fiber_lengths > ii);
+        YY_min = fiber_Ys_min(fiber_lengths > ii);
+        XX = fiber_Xs(fiber_lengths > ii);
+        LL = fiber_lengths(fiber_lengths > ii);
+    end
+    Y_max_mean(n) = mean(YY_max); Y_max_std(n) = std(YY_max);
+    Y_min_mean(n) = mean(YY_min); Y_min_std(n) = std(YY_min);
+    X_mean(n) = mean(XX); X_std(n) = std(XX);
+    L_mean(n) = mean(LL); L_std(n) = std(LL);
+    n=n+1;
+
+end
+
+errorbar(L_mean/lambda, atand(Y_max_mean./X_mean), atand(Y_max_std./X_mean), ...
+    atand(Y_max_std./X_mean), L_std/lambda, L_std/lambda, 'color', [217, 95, 2]/255, ...
+        'LineStyle', 'none', 'Marker', '^', 'MarkerSize', 10, 'LineWidth', 2);
+hold on;
+% % % errorbar(L_mean/lambda, atand(Y_min_mean./X_mean), atand(Y_min_std./X_mean), ...
+% % %     atand(Y_min_std./X_mean), L_std/lambda, L_std/lambda, 'ok', 'LineStyle', ...
+% % %     'none', 'LineWidth',2);
+% % % hold on;
+ylabel('$\beta\ (\mathrm{^\circ})$','FontSize', 24,'Interpreter', 'latex');
+xlabel('$L/\lambda$','FontSize', 24,'Interpreter', 'latex');
+xlim([0 3.3]); ylim([0 10])
+set(gca,'Box', 'On','XGrid', 'On', 'YGrid', 'On', 'GridAlpha', 0.5, 'FontSize', 24)
+
+legend({'$\lambda=30\mathrm{\mu m}$', ...
+    '$\lambda=30\mathrm{\mu m}$', ...
+    '$\lambda=45\mathrm{\mu m}$', ...
+    '$\lambda=45\mathrm{\mu m}$'},'Interpreter', 'latex', 'FontSize', 18)
+
+text(2.5, 0.7, '$\lambda/a=1.5$', 'FontSize', 24, 'FontWeight', 'bold', ...
+    'Interpreter','latex','BackgroundColor','w');
+
+f=gcf;
+exportgraphics(f,['E:\Processing & Results\Actin Filaments in Porous Media\' ...
+    'Figures\Actin-DLD\Beta_Length_errorbarPlot_exp20241031_exp20241111.png'],'Resolution',100)
+
+set(gcf,'renderer','Painters');
+print('-depsc2','-tiff','-r100','-vector',['E:\Processing & Results\Actin Filaments in Porous Media\' ...
+    'Figures\Actin-DLD\Beta_Length_errorbarPlot_exp20241031_exp20241111.eps']);
+
+
+
+%% bar plot and errorbar plot
+figure('Color','w','Position',[100 100 800 600]);
+fiber_Ys_max = fiber_Ys;
+fiber_Ys_min = fiber_Ys - flowfocusing_bandwidth;
+fiber_Ys_min(end-data_num+1:end) = fiber_Ys(end-data_num+1:end) - 200; % different band width: 200 um
+fiber_Ys_min(fiber_Ys_min < 0) = 0; % set value to zeros if it's negative
+beta_max = atand(fiber_Ys_max./fiber_Xs);
+beta_min = atand(fiber_Ys_min./fiber_Xs);
+
+plot([fiber_lengths'/lambda; fiber_lengths'/lambda], [beta_min'; beta_max'], 'color', [132 186 91]/255, 'LineWidth', 2);
+hold on;
+
+n = 1;
+for ii = 5:10:100
+
+    if ii < 80
+        YY_max = fiber_Ys_max(fiber_lengths > ii & fiber_lengths < ii+10);
+        YY_min = fiber_Ys_min(fiber_lengths > ii & fiber_lengths < ii+10);
+        XX = fiber_Xs(fiber_lengths > ii & fiber_lengths < ii+10);
+        LL = fiber_lengths(fiber_lengths > ii & fiber_lengths < ii+10);
+    else
+        YY_max = fiber_Ys_max(fiber_lengths > ii);
+        YY_min = fiber_Ys_min(fiber_lengths > ii);
+        XX = fiber_Xs(fiber_lengths > ii);
+        LL = fiber_lengths(fiber_lengths > ii);
+    end
+    Y_max_mean(n) = mean(YY_max); Y_max_std(n) = std(YY_max);
+    Y_min_mean(n) = mean(YY_min); Y_min_std(n) = std(YY_min);
+    X_mean(n) = mean(XX); X_std(n) = std(XX);
+    L_mean(n) = mean(LL); L_std(n) = std(LL);
+    n=n+1;
+
+end
+
+errorbar(L_mean/lambda, atand(Y_max_mean./X_mean), atand(Y_max_std./X_mean), ...
+    atand(Y_max_std./X_mean), L_std/lambda, L_std/lambda, 'ok', 'LineStyle', ...
+    'none', 'LineWidth',2);
+hold on;
+errorbar(L_mean/lambda, atand(Y_min_mean./X_mean), atand(Y_min_std./X_mean), ...
+    atand(Y_min_std./X_mean), L_std/lambda, L_std/lambda, 'ok', 'LineStyle', ...
     'none', 'LineWidth',2);
 hold on;
 % ylabel('$y\ (\mathrm{\mu m})$','FontSize', 24,'Interpreter', 'latex');
 % xlabel('$L\ (\mathrm{\mu m})$','FontSize', 24,'Interpreter', 'latex');
-ylabel('$\mathrm{Drift\,angle:}\,\sim\beta\ (\mathrm{^\circ})$','FontSize', 24,'Interpreter', 'latex');
-xlabel('$L/a$','FontSize', 24,'Interpreter', 'latex');
-xlim([0 5]); ylim([0 10])
+ylabel('$\beta\ (\mathrm{^\circ})$','FontSize', 24,'Interpreter', 'latex');
+xlabel('$L/\lambda$','FontSize', 24,'Interpreter', 'latex');
+xlim([0 3.3]); ylim([0 10])
 set(gca,'Box', 'On','XGrid', 'On', 'YGrid', 'On', 'GridAlpha', 0.5, 'FontSize', 24)
 
-%%% errorbar plot: overlap on the previous plot 
-n = 1;
-for ii = 5:10:100
-
-    if ii < 80
-        YY = fiber_Ys(fiber_lengths > ii & fiber_lengths < ii+10);
-        LL = fiber_lengths(fiber_lengths > ii & fiber_lengths < ii+10);
-    else
-        YY = fiber_Ys(fiber_lengths > ii);
-        LL = fiber_lengths(fiber_lengths > ii);
-    end
-    L_mean(n) = mean(LL); L_std(n) = std(LL);
-    Y_mean(n) = mean(YY); Y_std(n) = std(YY);
-    n=n+1;
-
-end
-
-plot(fiber_lengths/30, atand(fiber_Ys/4000), 'c.', 'MarkerSize', 20);
-hold on;
-errorbar(L_mean/30, atand(Y_mean/4000), atand(Y_std/4000), atand(Y_std/4000), L_std/30, L_std/30, '*b', 'LineStyle', ...
-    'none', 'LineWidth', 3);
-
-legend({'$a=20\mathrm{\mu m}, \lambda=30\mathrm{\mu m}$', ...
-    '$a=20\mathrm{\mu m}, \lambda=30\mathrm{\mu m}$', ...
-    '$a=30\mathrm{\mu m}, \lambda=45\mathrm{\mu m}$', ...
-    '$a=30\mathrm{\mu m}, \lambda=45\mathrm{\mu m}$'},'Interpreter', 'latex', 'FontSize', 18)
-
 f=gcf;
-exportgraphics(f,['F:\Processing & Results\Actin Filaments in Porous Media\' ...
-    'Figures\Actin-DLD\Beta_Length_errorbarPlot_exp20231030_exp20240415_exp20240522_newLegend.png'],'Resolution',100)
+exportgraphics(f,['E:\Processing & Results\Actin Filaments in Porous Media\' ...
+    'Figures\Actin-DLD\Beta_Length_errorbarPlot_exp20241031_plus_VeryLongOnes.png'],'Resolution',100)
 
 set(gcf,'renderer','Painters');
-print('-depsc2','-tiff','-r100','-vector',['F:\Processing & Results\' ...
-    'Actin Filaments in Porous Media\Figures\Actin-DLD\Beta_Length_errorbarPlot_exp20231030_exp20240415_exp20240522_newLegend.eps']);
+print('-depsc2','-tiff','-r100','-vector',['E:\Processing & Results\' ...
+    'Actin Filaments in Porous Media\Figures\Actin-DLD\Beta_Length_errorbarPlot_exp20241031_VeryLongOnes.eps']);
